44fbe0e9bd1fbf171250e025cfb3ad1fbf5a2c93
IrynaBaisangurova <92602692+IrynaBaisangurova@users.noreply.github.com>
2022-01-25 00:22:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+let firstVariable;
+
+firstVariable = 'Hello world!';
+
+console.log(firstVariable);
+let userName = 'Regina';
+console.log(userName);
+let surnameUser = 'German';
+console.log(surnameUser);
+let usersAge = 25;
+console.log(usersAge);
+
+let userNew = 'New User';
+console.log(userNew);
+
+let shoppingList = 'shopping list';
+console.log(shoppingList);
+
+let userOnline = 'user online';
+console.log(userOnline);
+
+let employeesSalary = 'employees salary';
+console.log(employeesSalary);
+
+let mouseCoordinates = 'cursor coordinates';
+console.log(mouseCoordinates);
+
+console.log(typeof 123);
+console.log(typeof 'test');
+
+let example;
+console.log('declare', typeof example);
+example = 10;
+console.log('init with number', typeof example);
+example = 'abc';
+console.log('init with string', typeof example);
+
